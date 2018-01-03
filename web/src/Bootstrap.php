be8dbfb8ce58541fa2998c6915002aeeebdce29d
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-01-03 19:11:17 +0530

    Added basic error handler & http reques/response handler
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- web/src/Bootstrap.php
+++ web/src/Bootstrap.php
@@ -1,3 +1,6 @@
 <?php declare(strict_types = 1);
 
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
 echo "Hello World from HelloFresh";
