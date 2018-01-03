64e224fceff421b56a130501f050b86b0a652f97
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-01-03 19:21:32 +0530

    Fast route added to manage routing
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- web/src/Bootstrap.php
+++ web/src/Bootstrap.php
@@ -3,8 +3,32 @@
 $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
 $response = new \Http\HttpResponse;
 
-$content = '<h1>Hello World</h1>';
-$response->setContent($content);
+$dispatcher = \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', function () {
+        echo 'Hello World';
+    });
+    $r->addRoute('GET', '/route', function () {
+        echo 'This works too';
+    });
+});
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPath());
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
+
 
 foreach ($response->getHeaders() as $header) {
     header($header, false);
