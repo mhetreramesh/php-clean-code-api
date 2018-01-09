2b5de2c748e63964925a556c82737783ae578c92
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-01-09 13:06:18 +0530

    Get Recipe Usecase Test Written
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- web/tests/Usecase/GetRecipeUsecaseTest.php
+++ web/tests/Usecase/GetRecipeUsecaseTest.php
@@ -15,7 +15,33 @@ class GetRecipeUsecaseTest extends TestCase
         $stub->method('execute')
              ->willReturn([]);
         $this->assertEquals([], $stub->execute(1));
+    }
 
+    public function testGetRecipeUsecaseFails()
+    {
+        $stub = $this->createMock(RecipeRepository::class);
+        $stub->method('find')
+             ->willReturn($this->getRecipe());
+        $result = (new GetRecipeUsecase($stub))->execute(3);
+        $this->assertNotEquals($this->getRecipe(), $result);
+    }
+
+    public function testGetRecipeUsecaseSuccess()
+    {
+        
+        $stub = $this->createMock(RecipeRepository::class);
+        $stub->method('find')
+             ->willReturn($this->getRecipe());
+        $result = (new GetRecipeUsecase($stub))->execute(1);
+        $this->assertEquals($this->getRecipe(), $result);
+    }
+
+    private function getRecipe()
+    {
+        $recipe = new \stdClass;
+        $recipe->id = 1;
+        $recipe->name = 'test recipe';
+        return $recipe;
     }
 }
 
