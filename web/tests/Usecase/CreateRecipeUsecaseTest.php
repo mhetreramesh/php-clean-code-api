2897877a21afeddad5a5afdc972c3e9653211ba9
Ramesh Mhetre <mhetreramesh@gmail.com>
2018-01-09 12:49:58 +0530

    Create recipe tests added' '
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- web/tests/Usecase/CreateRecipeUsecaseTest.php
+++ web/tests/Usecase/CreateRecipeUsecaseTest.php
@@ -6,6 +6,7 @@ use Core\Adapter\RecipeRepositoryInterface;
 use Core\Usecase\CreateRecipeUsecase;
 use PHPUnit\Framework\TestCase;
 use Core\Repository\RecipeRepository;
+use phpDocumentor\Reflection\Types\Object_;
 
 class CreateRecipeUsecaseTest extends TestCase
 {
@@ -15,6 +16,30 @@ class CreateRecipeUsecaseTest extends TestCase
         $stub->method('execute')
              ->willReturn([]);
         $this->assertEquals([], $stub->execute([]));
+    }
+
+    public function testCreateRecipeUsecaseCall()
+    {
+        $stub = $this->createMock(RecipeRepository::class);
+        $stub->method('save')
+             ->willReturn(1);
+        $stub->method('find')
+             ->willReturn([]);
+        $result = (new CreateRecipeUsecase($stub))->execute([]);     
+        $this->assertEquals([], $result);
+    }
+
+    public function testCreateRecipeUsecaseReturnsResult()
+    {
+        $recipe = new \stdClass;
+        $recipe->name = 'test recipe';
+        $stub = $this->createMock(RecipeRepository::class);
+        $stub->method('save')
+             ->willReturn(1);
+        $stub->method('find')
+             ->willReturn($recipe);
+        $result = (new CreateRecipeUsecase($stub))->execute([]);
+        $this->assertEquals($recipe, $result);
     }
 }
 
